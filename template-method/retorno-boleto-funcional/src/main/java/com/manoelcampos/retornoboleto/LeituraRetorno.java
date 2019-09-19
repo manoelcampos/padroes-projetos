b1dc95f746a32ec1d478fa6a6776939df3e80802
Manoel Campos <manoelcampos@gmail.com>
2019-09-19 07:08:08 -0300

    Adiciona projeto de ordenar lista com Strategy usando Comparator e method reference. Adiciona projeto que usa Template Method para evitar código repetido na implementação da leitura de retorno de boleto.

---
+++ template-method/retorno-boleto-funcional/src/main/java/com/manoelcampos/retornoboleto/LeituraRetorno.java
@@ -0,0 +1,33 @@
+package com.manoelcampos.retornoboleto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Manoel Campos da Silva Filho
+ */
+public interface LeituraRetorno {
+    DateTimeFormatter FORMATO_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static Boleto lerBancoBrasil(String[] vetor) {
+        Boleto boleto = new Boleto();
+        boleto.setId(Integer.parseInt(vetor[0]));
+        boleto.setCodBanco(vetor[1]);
+
+        boleto.setDataVencimento(LocalDate.parse(vetor[2], LeituraRetorno.FORMATO_DATA));
+        boleto.setDataPagamento(LocalDate.parse(vetor[3], LeituraRetorno.FORMATO_DATA).atTime(0, 0, 0));
+
+        boleto.setCpfCliente(vetor[4]);
+        boleto.setValor(Double.parseDouble(vetor[5]));
+        boleto.setMulta(Double.parseDouble(vetor[6]));
+        boleto.setJuros(Double.parseDouble(vetor[7]));
+        return boleto;
+    }
+}
