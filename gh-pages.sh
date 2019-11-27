7ad490acfc2df13110289ce25d0db9c0b51e6603
Manoel Campos <manoelcampos@gmail.com>
2019-11-27 15:18:23 -0300

    Adiciona gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git co master -B gh-pages || (echo "Não foi possível mudar para branch gh-pages" && exit -1)
+git checkout master -B gh-pages || (echo "Não foi possível mudar para branch gh-pages" && exit -1)
 find . -name "*.adoc" | xargs asciidoctor -b html 
 
 for FILE in `find . -name "README.html"`; do 
@@ -10,6 +10,6 @@ for FILE in `find . -name "*.html"`; do
    git add -f "$FILE"; 
 done
 
-git ci -m "Build AsciiDoc Files for GitHub Pages"
+git commit -m "Build AsciiDoc Files for GitHub Pages"
 git push -f --set-upstream origin gh-pages
-git co master
+git checkout master
