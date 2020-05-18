c727442683ad9ee0689db794e27e90439c0c31a3
Manoel Campos <manoelcampos@gmail.com>
2020-05-18 16:28:52 -0300

    Adiciona configurações para usar o plugin que permite controlar a apresentação remotamente (https://github.com/dubzzz/revealjs-remote).

---
+++ criacionais/builder/slides-plugins.js
@@ -0,0 +1,4 @@
+// https://github.com/dubzzz/revealjs-remote
+{ src: 'http://localhost:8080/socket.io/socket.io.js', async: true },
+{ src: 'http://localhost:8080/html2canvas.min.js', async: true },
+{ src: 'http://localhost:8080/listener.js', async: true }
