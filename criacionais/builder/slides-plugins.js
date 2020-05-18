e435b9f43eaaa25be1a14f0611189ef0baf7cd86
Manoel Campos <manoelcampos@gmail.com>
2020-05-18 16:30:17 -0300

    Remove configurações do plugin para controle remoto da apresentação.

--- criacionais/builder/slides-plugins.js
+++
@@ -1,4 +0,0 @@
-// https://github.com/dubzzz/revealjs-remote
-{ src: 'http://localhost:8080/socket.io/socket.io.js', async: true },
-{ src: 'http://localhost:8080/html2canvas.min.js', async: true },
-{ src: 'http://localhost:8080/listener.js', async: true }
