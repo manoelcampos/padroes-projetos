82032b82d908a27cec246ea768a70a71833a9bcc
Manoel Campos <manoelcampos@gmail.com>
2020-05-27 16:02:39 -0300

    Inclui projeto config-singleton-web-cdi com Quarkus, CDI, Swagger e Microprofile Config. Remove projeto cep-service-singleton com VRaptor.

---
+++ criacionais/singleton/config-singleton-web-cdi/src/main/java/com/manoelcampos/rest/ConfigResource.java
@@ -0,0 +1,43 @@
+package com.manoelcampos.rest;
+
+import com.manoelcampos.model.Config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Este é um Controller REST que fornece uma API para consulta
+ * de endereços a partir do CEP.
+ *
+ * <p>O framework Quarkus recomenda que os atributos injetados
+ * tenham visibilidade package, pois ele faz modificações
+ * no bytecode gerado.
+ * </p>
+ *
+ * @author Manoel Campos da Silva Filho
+ */
+@Path("/Config")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ConfigResource {
+    /**
+     * Injeta (instancia automaticamente) um objeto com as configurações
+     * da aplicação.
+     * Como a classe {@link Config} foi anotada com &#64;{@link ApplicationScoped},
+     * só existirá um único objeto (Singleton) config em toda a aplicação,
+     * independentemente de quantos usuários estejam conectados.
+     *
+     * O atributo é definido com visibilidade package por uma solicitação
+     * do Quarkus.
+     */
+    @Inject
+    Config config;
+
+    @GET
+    public Config getConfig() {
+        return config;
+    }
+
+}
