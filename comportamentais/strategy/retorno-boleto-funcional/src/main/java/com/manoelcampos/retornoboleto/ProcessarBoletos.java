551aa1486abb182789d93ebc9d6f6d3e8efe7881
Manoel Campos <manoelcampos@gmail.com>
2020-10-21 09:58:18 -0300

    Adiciona configurações no README.adoc do projeto do boleto funcional e link para vídeo

--- comportamentais/strategy/retorno-boleto-funcional/src/main/java/com/manoelcampos/retornoboleto/ProcessarBoletos.java
+++ comportamentais/strategy/retorno-boleto-funcional/src/main/java/com/manoelcampos/retornoboleto/ProcessarBoletos.java
@@ -41,7 +41,7 @@ public class ProcessarBoletos {
     private Function<String, List<Boleto>> leituraRetorno;
 
     public ProcessarBoletos(Function<String, List<Boleto>> leituraRetorno){
-        this.leituraRetorno = leituraRetorno;
+	 this.leituraRetorno = leituraRetorno;
     }
 
     /**
