88e56f31688f1860a4b8f0592ba6593fe0bca019
Manoel Campos <manoelcampos@gmail.com>
2023-04-24 09:15:36 -0300

    Refatora Esportador Problemático
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- criacionais/factory-method/exportador-problematico/src/main/java/Principal.java
+++ criacionais/factory-method/exportador-problematico/src/main/java/Principal.java
@@ -1,6 +1,5 @@
 import com.manoelcampos.exportador.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,20 +7,20 @@ import java.util.List;
  * @author Manoel Campos da Silva Filho
  */
 public class Principal {
-    private static final List<Usuario> USUARIOS = Arrays.asList(
+    private static final List<Usuario> USUARIOS = List.of(
             new Usuario("Manoel Campos da Silva Filho", "123.456.789-01", "Palmas"),
             new Usuario("Joana Brito", "444.555.666-77", "Porto Nacional"),
             new Usuario("Lúcia Alves", "999.888.777-66", "Palmas")
     );
 
-    private static final List<Produto> PRODUTOS = Arrays.asList(
+    private static final List<Produto> PRODUTOS = List.of(
             new Produto("TV", "LG", "132-A", 120),
             new Produto("Notebook", "Asus", "New age", 341),
             new Produto("Smartphone", "Samsung", "Galaxy S10", 214)
     );
 
     public static void main(String[] args) {
-        Principal principal = new Principal();
+        var principal = new Principal();
         principal.exportarUsuarios();
         principal.exportarProdutos();
     }
