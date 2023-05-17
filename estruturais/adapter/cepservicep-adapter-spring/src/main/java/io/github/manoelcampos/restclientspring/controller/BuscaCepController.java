be5bc839d46e67c32c069b9bdf67a8fc1425a7d6
Manoel Campos <manoelcampos@gmail.com>
2023-05-17 17:26:23 -0300

    Adiciona cepservicep-adapter-spring

---
+++ estruturais/adapter/cepservicep-adapter-spring/src/main/java/io/github/manoelcampos/restclientspring/controller/BuscaCepController.java
@@ -0,0 +1,40 @@
+package io.github.manoelcampos.restclientspring.controller;
+
+import io.github.manoelcampos.restclientspring.model.Endereco;
+import io.github.manoelcampos.restclientspring.service.BuscaCepService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller que intermedia o acesso a um serviço de CEP.
+ * Ele recebe as requisições e encaminho para o serviço de
+ * CEP atualmente configurado.
+ *
+ * <p>Assim, caso deseje-se mudar o serviço utilizado,
+ * aplicações clientes que estejam acessando este controller REST
+ * não precisam alterar o endereço do serviço de CEP
+ * (pois tais clientes não se comunicam diferente com o serviço de CEP).
+ * </p>
+ * @author Manoel Campos da Silva Filho
+ */
+@RestController
+@RequestMapping("/cep")
+public class BuscaCepController {
+    @Value("buscacep.baseurl")
+    private String cepServiceBaseUrl;
+    private final BuscaCepService cepService;
+
+    public BuscaCepController(final BuscaCepService cepService) {
+        this.cepService = cepService;
+    }
+
+    @GetMapping("/{cep}")
+    public Endereco buscaEndereco(@PathVariable String cep){
+        System.out.println("Fazendo busca de CEP usando serviço " + cepServiceBaseUrl);
+        return cepService.buscaEndereco(cep);
+    }
+
+}
