b5d423a80691291624bed18269fee884af9c0b49
Manoel Campos <manoelcampos@gmail.com>
2023-05-17 18:31:31 -0300

    Atualiza documentação do cepservice-adapter-spring-automatico
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- estruturais/adapter/cepservicep-adapter-spring-automatico/src/main/java/io/github/manoelcampos/restclientspring/service/BuscaCepService.java
+++ estruturais/adapter/cepservicep-adapter-spring-automatico/src/main/java/io/github/manoelcampos/restclientspring/service/BuscaCepService.java
@@ -11,9 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
  * A classe SpringApplication do seu projeto deve ser anotada
  * com {@link org.springframework.cloud.openfeign.EnableFeignClients}.
  *
+ * A URL pode ser configurada por meio
+ * da propriedade spring.cloud.openfeign.client.config.feignName.url,
+ * onde feignName é o valor da propriedade name nesta interface.
+ *
  * @author Manoel Campos da Silva Filho
  */
-@FeignClient(name = "busca-cep", url = "${buscacep.baseurl}")
+@FeignClient(name = "buscacep", url = "${buscacep.baseurl}")
 public interface BuscaCepService {
     @GetMapping("${buscacep.buscaEnderecoEndpoint}")
     Endereco buscaEndereco(@PathVariable String cep);
