16acd0472762dfb1880c972e7d2326c0d14bbe85
Manoel Campos <manoelcampos@gmail.com>
2023-06-21 17:16:22 -0300

    Atualiza exercícios

---
+++ exercicios/real-random-number-services/src/main/java/io/github/manoelcampos/realrandom/Principal.java
@@ -0,0 +1,11 @@
+package io.github.manoelcampos.realrandom;
+
+/**
+ * Classe principal para rodar e "testar" sua implementação.
+ */
+public class Principal {
+    public static void main(final String[] args) {
+        //Insira seu código para "teste" aqui
+        System.err.println("Você precisa implementar o método main");
+    }
+}
