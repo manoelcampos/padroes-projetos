46581a21a5be3e97e4114320f3bb409e8f398ab0
Manoel Campos <manoelcampos@gmail.com>
2024-08-12 11:14:28 -0300

    Renomeia aplicação sem adapters de captcha
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ estruturais/01-adapter/captcha-v1-sem-adapter/src/main/java/io/github/manoelcampos/captchaadapters/CaptchaApplication.java
@@ -0,0 +1,13 @@
+package io.github.manoelcampos.captchaadapters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchaApplication.class, args);
+	}
+
+}
