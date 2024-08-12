93f3c8d2750f7759bd438a9cf9222fbb49f589bd
Manoel Campos <manoelcampos@gmail.com>
2024-08-12 11:09:24 -0300

    Aplicação sem adapters de captcha

---
+++ estruturais/01-adapter/captcha-adapters-v1-sem-adapter/src/main/java/io/github/manoelcampos/captchaadapters/controllers/CaptchaController.java
@@ -0,0 +1,30 @@
+package io.github.manoelcampos.captchaadapters.controllers;
+
+import com.mewebstudio.captcha.Captcha;
+import com.mewebstudio.captcha.GeneratedCaptcha;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.SessionScope;
+
+/**
+ * @author Manoel Campos
+ */
+@RestController
+@RequestMapping("/captcha")
+@SessionScope
+public class CaptchaController {
+    private GeneratedCaptcha generatedCaptcha;
+
+    @GetMapping(value = "/gerar", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] gerar(){
+        this.generatedCaptcha = new Captcha().generate();
+        final byte[] image = ImageUtil.bufferedImageToByteArray(generatedCaptcha.getImage());
+        System.out.println(generatedCaptcha.getCode());
+        return image;
+    }
+
+    @GetMapping("/confirmar/{codigo}")
+    public boolean confirmar(@PathVariable final String codigo){
+        return generatedCaptcha != null && codigo.equals(generatedCaptcha.getCode());
+    }
+}
